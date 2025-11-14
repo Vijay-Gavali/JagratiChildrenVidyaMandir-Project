51c0434e2434723b1c10e1cfb3e22b2dc06d988e
vijay gavali <vijaygawli37@gmail.com>
2025-11-14 13:18:58 +0530

    Initial Spring Boot project upload

---
+++ src/main/java/com/jagratichildrenvidyamandir/JagratiChildrenVidyaMandirProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jagratichildrenvidyamandir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagratiChildrenVidyaMandirProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JagratiChildrenVidyaMandirProjectApplication.class, args);
+	}
+
+}
